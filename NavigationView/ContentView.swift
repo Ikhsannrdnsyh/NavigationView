e19c503e247cf85dec3bb8fc86fdf3abaeece6a1
ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2023-06-12 13:54:12 +0700

    first commit

--- NavigationView/ContentView.swift
+++ NavigationView/ContentView.swift
@@ -9,15 +9,91 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            firstScreen(image: "house.circle", text: "Home Screen", title: "Home")
+            
         }
-        .padding()
+        
     }
 }
+struct firstScreen:View{
+    let image: String
+    let text: String
+    let title: String
+    
+    var body: some View{
+        VStack{
+            Image(systemName: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200,
+                       height: 200,
+                       alignment: .center)
+                .padding()
+            
+            Text(text)
+                .font(.system(size: 30, weight: .light, design: .default))
+                .padding()
+            
+            NavigationLink(destination: SecondScreen(), label: {
+                    ContinueButton()
+                    
+            }
+            )
+        }
+        .navigationTitle(title)
+        .navigationBarTitleDisplayMode(.inline)
+        
+    }
+}
+
+
+struct SecondScreen:View{
+    var body: some View {
+        VStack{
+            Image(systemName: "person.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200,
+                       height: 200,
+                       alignment: .center)
+                .padding()
+            
+            Text("Second Screen")
+                .font(.system(size: 30, weight: .light, design: .default))
+                .padding()
+            
+            
+        }
+        .navigationTitle("profile")
+        .navigationBarTitleDisplayMode(.inline)
+        
+    }
+        
+    
+}
+
+
+struct ContinueButton: View{
+    var body: some View{
+        Text("Continue")
+            .frame(width: 200, height: 50, alignment: .center)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+    }
+}
+
+struct backButton: View{
+    var body: some View{
+        Text("Back")
+            .frame(width: 200, height: 50, alignment: .center)
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
