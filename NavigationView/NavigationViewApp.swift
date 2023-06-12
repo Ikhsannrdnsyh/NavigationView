8be8c8bddb2b1ebeab28f39285edd9874eeac531
ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2023-06-12 12:58:51 +0700

    Initial Commit

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
